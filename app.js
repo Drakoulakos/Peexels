44053a75a5d2f1b332fc812d8aae08087f3fad5d
Michalis Drakoulakos <mikerx87@gmail.com>
2023-05-20 13:23:31 +0300

    add video button

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// Get the search input and search button elements
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const resultsContainer = document.getElementById("resultsContainer");
+// Image Search
+const imageSearchInput = document.getElementById("searchInput");
+const imageSearchButton = document.getElementById("imageSearchButton");
+const imageResultsContainer = document.getElementById("resultsContainer");
 
-// Add event listeners
-searchButton.addEventListener("click", searchImages);
-searchInput.addEventListener("keyup", handleKeyPress);
-document.getElementById("clearButton").addEventListener("click", function () {
-  resultsContainer.innerHTML = "";
-});
+imageSearchButton.addEventListener("click", searchImages);
+imageSearchInput.addEventListener("keyup", handleImageKeyPress);
+document
+  .getElementById("clearImageButton")
+  .addEventListener("click", clearImageResults);
 
-// Function to handle image search
 function searchImages() {
-  const query = searchInput.value;
+  const query = imageSearchInput.value;
 
   if (!query) {
-    displayErrorMessage("Please type something");
+    displayImageErrorMessage("Please type something");
     return;
   }
 
-  // Make a request to the Pexels API
   fetch(`https://api.pexels.com/v1/search?query=${query}`, {
     headers: {
       Authorization: "d3mgFTRyPdzTWXs6HIkOxRwxiAQ2ZCwWCzsm15yu0BnrjHicNa6BajF7", // Replace with your API key
@@ -28,42 +25,103 @@ function searchImages() {
     .then((response) => response.json())
     .then((data) => {
       if (data.photos.length === 0) {
-        displayNoResultsMessage();
+        displayNoImageResultsMessage();
       } else {
         displayImages(data.photos);
       }
     });
 }
 
-// Function to display a message for empty results
-function displayNoResultsMessage() {
-  resultsContainer.innerHTML = "No results found.";
+function clearImageResults() {
+  imageResultsContainer.innerHTML = "";
 }
 
-// Function to display an error message
-function displayErrorMessage(message) {
-  resultsContainer.innerHTML = message;
+function displayNoImageResultsMessage() {
+  imageResultsContainer.innerHTML = "No results found.";
 }
 
-// Function to display the images
+function displayImageErrorMessage(message) {
+  imageResultsContainer.innerHTML = message;
+}
+
 function displayImages(photos) {
-  // Clear previous search results
-  resultsContainer.innerHTML = "";
+  clearImageResults();
 
-  // Loop through the photos and create image elements
   photos.forEach((photo) => {
     const imageElement = document.createElement("img");
     imageElement.src = photo.src.large;
     imageElement.alt = photo.photographer;
-    resultsContainer.appendChild(imageElement);
+    imageResultsContainer.appendChild(imageElement);
   });
 }
 
-// Function to handle key press event
-function handleKeyPress(event) {
-  // Check if the pressed key is Enter (key code 13)
+function handleImageKeyPress(event) {
   if (event.keyCode === 13) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
     searchImages();
+  }
+}
+
+// Video Search
+const videoSearchInput = document.getElementById("videoSearchInput");
+const videoSearchButton = document.getElementById("videoSearchButton");
+const videoResultsContainer = document.getElementById("videoResultsContainer");
+
+videoSearchButton.addEventListener("click", searchVideos);
+videoSearchInput.addEventListener("keyup", handleVideoKeyPress);
+document
+  .getElementById("clearVideoButton")
+  .addEventListener("click", clearVideoResults);
+
+function searchVideos() {
+  const query = videoSearchInput.value;
+
+  if (!query) {
+    displayVideoErrorMessage("Please enter a search term");
+    return;
+  }
+
+  fetch(`https://api.pexels.com/videos/search?query=${query}`, {
+    headers: {
+      Authorization: "d3mgFTRyPdzTWXs6HIkOxRwxiAQ2ZCwWCzsm15yu0BnrjHicNa6BajF7", // Replace with your API key
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.videos.length === 0) {
+        displayNoVideoResultsMessage();
+      } else {
+        displayVideos(data.videos);
+      }
+    });
+}
+
+function clearVideoResults() {
+  videoResultsContainer.innerHTML = "";
+}
+
+function displayNoVideoResultsMessage() {
+  videoResultsContainer.innerHTML = "No video results found.";
+}
+
+function displayVideoErrorMessage(message) {
+  videoResultsContainer.innerHTML = message;
+}
+
+function displayVideos(videos) {
+  clearVideoResults();
+
+  videos.forEach((video) => {
+    const videoElement = document.createElement("video");
+    videoElement.src = video.video_files[0].link;
+    videoElement.controls = true;
+    videoResultsContainer.appendChild(videoElement);
+  });
+}
+
+function handleVideoKeyPress(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchVideos();
   }
 }
